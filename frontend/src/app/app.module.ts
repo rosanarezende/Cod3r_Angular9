8f5ffd2a39b3bbf03816e3f6b986d3ccf20296da
Rosana Rezende <rezende_rosana@hotmail.com>
2020-08-15 13:40:22 -0300

    cria diretiva de atributo

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -10,12 +10,15 @@ import { FooterComponent } from './components/template/footer/footer.component';
 import { NavComponent } from './components/template/nav/nav.component';
 
 import { HomeComponent } from './views/home/home.component';
+import { ProductCrudComponent } from './views/product-crud/product-crud.component';
 
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatSidenavModule } from  '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
 import { MatCardModule } from  '@angular/material/card';
-import { ProductCrudComponent } from './views/product-crud/product-crud.component';
+
+
+import { RedDirective } from './directives/red.directive';
 
 @NgModule({
   declarations: [
@@ -24,7 +27,8 @@ import { ProductCrudComponent } from './views/product-crud/product-crud.componen
     FooterComponent,
     NavComponent,
     HomeComponent,
-    ProductCrudComponent
+    ProductCrudComponent,
+    RedDirective
   ],
   imports: [
     BrowserModule,
